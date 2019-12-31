a1eb8d0f63ac96f4c45445a49a0380b1e6110d9e
rusdevops <rusdevops@gmail.com>
2019-12-31 13:20:39 +0300

    fixed file duplication for install.sh by symlinks

--- linuxbrew/centos-8/export.sh
+++
@@ -1,4 +0,0 @@
-export PATH=/home/developer/.linuxbrew/bin:/home/developer/.linuxbrew/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export SHELL=/bin/bash
-eval $(~/.linuxbrew/bin/brew shellenv)
-brew config >/dev/null 2>&1
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-mkdir ~/.linuxbrew/bin
-ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+git clone https://github.com/Homebrew/brew $HOME/.linuxbrew/Homebrew
+mkdir $HOME/.linuxbrew/bin
+ln -s $HOME/.linuxbrew/Homebrew/bin/brew $HOME/.linuxbrew/bin
+
+eval $($HOME/.linuxbrew/bin/brew shellenv)
+test -r $HOME/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>$HOME/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>$HOME/.profile
