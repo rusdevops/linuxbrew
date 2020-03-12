4b814f565e5caaf9232ae1999db6e17fe73c597b
rusdevops <rusdevops@gmail.com>
2020-03-12 22:36:17 +0300

    fixed init script of linuxbrew

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,5 +8,5 @@ mkdir $HOME/.linuxbrew/bin
 ln -s $HOME/.linuxbrew/Homebrew/bin/brew $HOME/.linuxbrew/bin
 
 eval $($HOME/.linuxbrew/bin/brew shellenv)
-test -r $HOME/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>$HOME/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>$HOME/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>$HOME/.profile
