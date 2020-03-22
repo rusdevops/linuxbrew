092992999d9f0d90442a99d6a11515d34e9ce599
rusdevops <rusdevops@gmail.com>
2020-03-22 11:08:47 +0300

    add brew config for force load core components

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ ln -s $HOME/.linuxbrew/Homebrew/bin/brew $HOME/.linuxbrew/bin
 eval $($HOME/.linuxbrew/bin/brew shellenv)
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>$HOME/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>$HOME/.profile
+
+brew config
