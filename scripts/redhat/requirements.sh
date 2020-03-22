f3c8986c7d4c3decaa4fb662be097a21f65b7917
rusdevops <rusdevops@gmail.com>
2020-03-22 20:30:53 +0300

    added fix for fedora

--- scripts/redhat/requirements.sh
+++ scripts/redhat/requirements.sh
@@ -5,4 +5,5 @@ set -ex
 yum -y --nogpgcheck install which
 yum -y groupinstall 'Development Tools'
 yum -y install curl file git
+yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
 yum clean all
