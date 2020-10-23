f3059c08902cb98801259627f4500b19efa357ee
rusdevops <rusdevops@gmail.com>
2020-10-23 13:12:16 +0300

    Update requirements.sh

--- scripts/redhat/requirements.sh
+++ scripts/redhat/requirements.sh
@@ -11,7 +11,10 @@ yum -y install curl \
                perl-Module-Load-Conditional \
                perl-core \
                gnupg2
+
 dnf -y install gcc-toolset-9-gcc gcc-toolset-9-gcc-c++
+source /opt/rh/gcc-toolset-9/enable
+ln -s /opt/rh/gcc-toolset-9/root/usr/bin/gcc /usr/bin/gcc-9
 
 yum clean all
 dnf clean all
