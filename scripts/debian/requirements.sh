07c04ada9a1c81196c3eb1880c60588f86540c0c
rusdevops <rusdevops@gmail.com>
2020-10-23 13:18:34 +0300

    Update requirements.sh

--- scripts/debian/requirements.sh
+++ scripts/debian/requirements.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
-echo 'deb http://deb.debian.org/debian testing main' >> /etc/apt/sources.list
-apt update -y
-apt install -y gcc
+if [ -f "/etc/debian_version" ]; then
+  echo 'deb http://deb.debian.org/debian testing main' >> /etc/apt/sources.list
+  apt update -y
+fi
 
 apt update -y
 apt install -y gcc-9 build-essential curl file git gnupg2
