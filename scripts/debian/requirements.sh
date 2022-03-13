380236bfdb0c6d4d2cc03824834238a7f0350fd1
rusdevops <rusdevops@gmail.com>
2022-03-13 16:37:20 +0300

    updated gcc

--- scripts/debian/requirements.sh
+++ scripts/debian/requirements.sh
@@ -6,5 +6,5 @@ apt update -y
 apt install -y build-essential curl file git gnupg2
 echo 'deb http://deb.debian.org/debian testing main' >> /etc/apt/sources.list
 apt update -y
-apt install -y gcc-9 
+apt install -y gcc
 rm -rf /var/lib/apt/lists/*
